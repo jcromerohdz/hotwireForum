f37cfb9161b61963967b2bd67c5353163274a45c
ITT <jcromerohdz@gmail.com>
2022-11-04 12:43:33 -0700

    Edit discussions action

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,5 +1,6 @@
 class DiscussionsController < ApplicationController
    before_action :authenticate_user! 
+   before_action :set_discussion, only: [:edit, :update] 
 
    def index
     @discussions = Discussion.all
@@ -11,7 +12,6 @@ class DiscussionsController < ApplicationController
 
    def create
     @discussion = Discussion.new(discussion_params)
-
     respond_to do |format|
       if @discussion.save
         format.html { redirect_to discussions_path, notice: "Discussion created"}
@@ -19,12 +19,28 @@ class DiscussionsController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
       end
     end
+   end
 
+   def edit
+   end
+
+   def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to discussions_path, notice: "Discussion updated"}
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
    end
 
    private
 
    def discussion_params
     params.require(:discussion).permit(:name, :closed, :pinned)
+   end
+
+   def set_discussion
+    @discussion = Discussion.find(params[:id])
    end
 end
